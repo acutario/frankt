601a854fa3786206f9a9d07ca5e3bc7b96262d57
belaustegui <cristian.alvb@gmail.com>
2018-07-03 20:21:08 +0200

    Connect to a single channel each time
    For keeping the examples as short as possible, we are connecting to a
    single channel each time. The channel name is provided by the rendered
    view name, so it can be customized per-page.

--- test/support/test_application_web/assets/app.js
+++ test/support/test_application_web/assets/app.js
@@ -11,19 +11,4 @@ window.onload = function () {
     .receive("ok", (res) => {
       console.log('do your magic!', res);
     });
-
-  // Chat demo
-  const chat = document.querySelector("div.chat");
-
-  if (chat) {
-    chat.insertAdjacentHTML('beforeend', '<p><i>Connecting...</i></p>');
-    let chat_channel = Frankt.join("chat:lobby")
-      .receive("ok", (res) => {
-        chat.insertAdjacentHTML('beforeend', '<p><i>Connected!</i></p>');
-      });
-
-    chat_channel.channel.on("message", (res) => {
-      chat.insertAdjacentHTML('beforeend', `<p><b>${res.sender}:</b> ${res.message}</p>`);
-    });
-  }
 }
--- test/support/test_application_web/views/chat_view.ex
+++ test/support/test_application_web/views/chat_view.ex
@@ -1,3 +1,5 @@
 defmodule Frankt.TestApplicationWeb.ChatView do
   use Frankt.TestApplicationWeb, :view
+
+  def frankt_channel(_assigns), do: tag(:meta, name: "channel", content: "chat:lobby")
 end
